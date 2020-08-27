6f70418983b1c34b8db7bc1119f449569f17e2c7
Jorge Luis Palacios Dalo <jorge.palacios@skyairline.com>
2020-08-27 14:38:51 -0400

    Updates

---
+++ precache-manifest.7beefc7e5b593fcc969333c86a7b226b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e38840724f71f9836528",
+    "url": "/react-badges/static/css/main.32f06ce4.chunk.css"
+  },
+  {
+    "revision": "e38840724f71f9836528",
+    "url": "/react-badges/static/js/main.e3884072.chunk.js"
+  },
+  {
+    "revision": "1ca7e770b323ef47f898",
+    "url": "/react-badges/static/js/runtime~main.1ca7e770.js"
+  },
+  {
+    "revision": "1e80e2accd58177ba7a4",
+    "url": "/react-badges/static/css/2.efdaef25.chunk.css"
+  },
+  {
+    "revision": "1e80e2accd58177ba7a4",
+    "url": "/react-badges/static/js/2.1e80e2ac.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "86d4890e409e0f21a36487776a86ec12",
+    "url": "/react-badges/static/media/500-Server-Error.86d4890e.svg"
+  },
+  {
+    "revision": "f55f72ff8a14fd0c88d49c1c854bc177",
+    "url": "/react-badges/static/media/go-back-button.f55f72ff.svg"
+  },
+  {
+    "revision": "be3b182178774bd48f777ecb58759bdb",
+    "url": "/react-badges/static/media/404-error-page.be3b1821.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "7147f0c6495c67ce7d93cfed7b39ee9b",
+    "url": "/react-badges/index.html"
+  }
+];
